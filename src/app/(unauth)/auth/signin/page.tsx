390e45305601ec8668a9995bdaa16d6a1bf89c96
levanduan997 <duanlv@rikkeisoft.com>
2024-09-13 07:19:45 +0900

    update file

--- src/app/(unauth)/auth/signin/page.tsx
+++ src/app/(unauth)/auth/signin/page.tsx
@@ -1,21 +1,25 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-"use client";
-import { useEffect, useState } from 'react';
-import { signIn, getProviders } from 'next-auth/react';
-import Image from 'next/image';
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// "use client";
+//import { useEffect, useState } from 'react';
+// import { signIn, getProviders } from 'next-auth/react';
+// import Image from 'next/image';
 
-export default function SignIn() {
-  const [providers, setProviders] = useState<any>(null);
+import SignIn from "@/components/sign-in";
 
-  useEffect(() => {
-    const setTheProviders = async () => {
-      const setupProviders = await getProviders();
-      setProviders(setupProviders);
-    };
-    setTheProviders();
-  }, []);
 
-  console.log({ providers });
+
+export default function SignInPage() {
+  // const [providers, setProviders] = useState<any>(null);
+
+  // useEffect(() => {
+  //   const setTheProviders = async () => {
+  //     const setupProviders = await getProviders();
+  //     setProviders(setupProviders);
+  //   };
+  //   setTheProviders();
+  // }, []);
+
+  // console.log({ providers });
   
 
   return (
@@ -23,11 +27,12 @@ export default function SignIn() {
       <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
         <h3 className="text-2xl font-bold text-center">Login to your account</h3>
         <div className="mt-4">
-          {providers &&
+         <SignIn />
+          {/* {providers &&
             Object.values(providers).map((provider: any) => (
               <div key={provider.name} className="flex items-center justify-center">
                 <button
-                  onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+                  onClick={() => signIn(provider.id)}
                   className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
                 >
                   <Image
@@ -39,7 +44,7 @@ export default function SignIn() {
                   <span>Sign in with {provider.name}</span>
                 </button>
               </div>
-            ))}
+            ))} */}
         </div>
       </div>
     </div>
